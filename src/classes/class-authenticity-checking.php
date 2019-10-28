d46a6892a5375d34a91f5f25461f3907f514302f
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2019-10-28 17:22:43 -0300

    Fix ip comparing mechanism

--- src/classes/class-authenticity-checking.php
+++ src/classes/class-authenticity-checking.php
@@ -48,7 +48,7 @@ if ( ! class_exists( 'ThanksToIT\ERWC\Authenticity_Checking' ) ) {
 				'default'           => 'yes',
 				//'checking_status_id' => get_option( 'erwc_opt_checking_email_status', ERWC()->factory->get_referral_checking_tax()->get_probably_email_checking_id() )
 			);
-			/*$checking_methods[] = array(
+			$checking_methods[] = array(
 				'id'                   => 'ip_comparing',
 				'disable'              => apply_filters( 'erwc_is_free_version', true ),
 				'hide_checking_result' => apply_filters( 'erwc_is_free_version', true ),
@@ -56,7 +56,7 @@ if ( ! class_exists( 'ThanksToIT\ERWC\Authenticity_Checking' ) ) {
 				'desc'                 => __( 'Checks if Referrer and Referee IPs are identical.', 'easy-referral-for-woocommerce' ),
 				'title'                => __( 'IP Comparing', 'easy-referral-for-woocommerce' ),
 				'default_status_id'    => get_option( 'erwc_opt_ip_comparing_status_default' )
-			);*/
+			);
 			return $checking_methods;
 		}
 
