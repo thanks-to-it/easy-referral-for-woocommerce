6e5170485d31e5f2bc229c997ec8bf41199338c3
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2020-07-09 19:27:51 -0300

    Test

--- src/app/class-functions.php
+++ src/app/class-functions.php
@@ -9,20 +9,6 @@
 
 namespace ThanksToIT\ERWC;
 
-use ThanksToIT\ERWC\Admin\Admin_Settings_Authenticity;
-use ThanksToIT\ERWC\Admin\Admin_Settings_Codes;
-use ThanksToIT\ERWC\Admin\Admin_Settings_General;
-use ThanksToIT\ERWC\Admin\Admin_Settings_Interface;
-use ThanksToIT\ERWC\Admin\Referral_Code_Admin_Settings;
-use ThanksToIT\ERWC\Referral\Referral_CPT;
-use ThanksToIT\ERWC\My_Account\Referral_Codes_Tab;
-use ThanksToIT\ERWC\My_Account\Referral_Tab;
-use ThanksToIT\ERWC\Referral\Referral_Authenticity_Tax;
-use ThanksToIT\ERWC\Referral\Referral_Checking_Tax;
-use ThanksToIT\ERWC\Referral\Referral_Email;
-use ThanksToIT\ERWC\Referral\Referral_Meta;
-use ThanksToIT\ERWC\Referral\Referral_Status_Tax;
-
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 } // Exit if accessed directly
@@ -37,7 +23,7 @@ if ( ! class_exists( 'ThanksToIT\ERWC\Functions' ) ) {
 			$vars_modified = array_map( function ( $item ) {
 				return '<code>{{' . $item . '}}</code>';
 			}, $vars );
-			return $args['text_before'] . ' ' . implode( ", ", $vars_modified );
+			return '<p class="description">'.$args['text_before'] . ' ' . implode( ", ", $vars_modified ).'</p>';
 		}
 	}
 }
